401c6c2f7b22d7c815b232ebb63d3c2c8598a966
Zsolt Molnar <molnar.zsolt@magyarbankholding.hu>
2023-04-16 19:38:18 +0200

    Update divine protection implementation

--- common/src/main/java/net/paladins/effect/DivineProtectionStatusEffect.java
+++ common/src/main/java/net/paladins/effect/DivineProtectionStatusEffect.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
 public class DivineProtectionStatusEffect extends StatusEffect {
+    public static final float multiplier = 0.5F;
     public DivineProtectionStatusEffect(StatusEffectCategory statusEffectCategory, int color) {
         super(statusEffectCategory, color);
     }
--- common/src/main/java/net/paladins/mixin/effect/LivingEntityDivineProtection.java
+++ common/src/main/java/net/paladins/mixin/effect/LivingEntityDivineProtection.java
@@ -2,23 +2,24 @@ package net.paladins.mixin.effect;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.paladins.effect.DivineProtectionStatusEffect;
 import net.paladins.effect.Effects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityDivineProtection {
-    @Inject(method = "modifyAppliedDamage", at = @At("RETURN"), cancellable = true)
-    private void modifyAppliedDamage_DivineProtection(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
+
+    // Mixin magic by TelepathicGrunt
+    // When `argsOnly = true` is passed, ModifyVariable infers which argument we want to modify
+    // based on the return value type
+    @ModifyVariable(method = "modifyAppliedDamage", at = @At("HEAD"), argsOnly = true)
+    private float modifyAppliedDamage_DivineProtection(float amount, DamageSource source) {
         LivingEntity entity = (LivingEntity) (Object) this;
-
         if (source.isUnblockable() || !entity.hasStatusEffect(Effects.DIVINE_PROTECTION)) {
-            return;
+            return amount;
         }
-
-        float returnValue = cir.getReturnValue();
-        cir.setReturnValue(returnValue * 0.5F);
+        return amount * DivineProtectionStatusEffect.multiplier;
     }
 }

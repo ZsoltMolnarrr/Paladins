5eac1408ef630699d471cce9996a82908f99d040
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-10-20 00:34:12 +0200

    Add priest trims

--- common/src/main/java/net/paladins/client/armor/PaladinArmorRenderer.java
+++ common/src/main/java/net/paladins/client/armor/PaladinArmorRenderer.java
@@ -8,20 +8,20 @@ import net.paladins.PaladinsMod;
 
 public class PaladinArmorRenderer extends AzArmorRenderer {
     public static PaladinArmorRenderer paladin() {
-        return new PaladinArmorRenderer("paladin_armor", "paladin_armor", "paladin_armor_trim");
+        return new PaladinArmorRenderer("paladin_armor", "paladin_armor");
     }
     public static PaladinArmorRenderer crusader() {
-        return new PaladinArmorRenderer("paladin_armor", "crusader_armor", "crusader_armor_trim");
+        return new PaladinArmorRenderer("paladin_armor", "crusader_armor");
     }
     public static PaladinArmorRenderer netheriteCrusader() {
-        return new PaladinArmorRenderer("paladin_armor", "netherite_crusader_armor", "crusader_armor_trim");
+        return new PaladinArmorRenderer("paladin_armor", "netherite_crusader_armor");
     }
 
-    public PaladinArmorRenderer(String modelName, String textureName, String trimTexture) {
+    public PaladinArmorRenderer(String modelName, String textureName) {
         super(AzArmorRendererConfig.builder(
                 Identifier.of(PaladinsMod.ID, "geo/" + modelName + ".geo.json"),
                 Identifier.of(PaladinsMod.ID, "textures/armor/" + textureName + ".png"))
-                .addRenderLayer(new AzArmorTrimLayer(Identifier.of(PaladinsMod.ID, "armor/trim/" + trimTexture), false))
+                .addRenderLayer(new AzArmorTrimLayer(Identifier.of(PaladinsMod.ID, "armor/trim/" + textureName + "_generic"), false))
                 .build());
     }
 }
--- common/src/main/java/net/paladins/client/armor/PriestArmorRenderer.java
+++ common/src/main/java/net/paladins/client/armor/PriestArmorRenderer.java
@@ -2,6 +2,7 @@ package net.paladins.client.armor;
 
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelibarmor.rewrite.render.layer.AzArmorTrimLayer;
 import net.minecraft.util.Identifier;
 import net.paladins.PaladinsMod;
 
@@ -19,7 +20,9 @@ public class PriestArmorRenderer extends AzArmorRenderer {
     public PriestArmorRenderer(String modelName, String textureName) {
         super(AzArmorRendererConfig.builder(
                 Identifier.of(PaladinsMod.ID, "geo/" + modelName + ".geo.json"),
-                Identifier.of(PaladinsMod.ID, "textures/armor/" + textureName + ".png")
-        ).build());
+                Identifier.of(PaladinsMod.ID, "textures/armor/" + textureName + ".png"))
+                .addRenderLayer(new AzArmorTrimLayer(Identifier.of(PaladinsMod.ID, "armor/trim/" + textureName + "_generic"), false))
+                .build()
+        );
     }
 }

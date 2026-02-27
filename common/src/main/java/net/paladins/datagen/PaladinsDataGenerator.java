c6d537ac76cc070515d07f07b34ccf882a71d03d
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2026-02-27 18:13:17 +0100

    Update stuff

--- common/src/main/java/net/paladins/datagen/PaladinsDataGenerator.java
+++ common/src/main/java/net/paladins/datagen/PaladinsDataGenerator.java
@@ -24,6 +24,7 @@ import net.spell_engine.rpg_series.item.Armor;
 import net.spell_engine.rpg_series.datagen.RPGSeriesDataGen;
 import net.spell_engine.rpg_series.tags.RPGSeriesItemTags;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -62,12 +63,17 @@ public class PaladinsDataGenerator implements DataGeneratorEntrypoint {
         @Override
         protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
             var namespace = PaladinsMod.ID;
+            var treasureTagBuilder = getOrCreateTagBuilder(SpellTags.TREASURE);
+            var processedBooks = new HashSet<PaladinSpells.Book>();
             PaladinSpells.entries.forEach(entry -> {
                 if (entry.book() != null) {
                     var bookTagKey = SpellTags.spellBook(namespace, entry.book().toString().toLowerCase());
                     getOrCreateTagBuilder(bookTagKey).addOptional(entry.id());
                     var scrollTagKey = SpellTags.spellScroll(namespace, entry.book().toString().toLowerCase());
                     getOrCreateTagBuilder(scrollTagKey).addOptional(entry.id());
+                    if (processedBooks.add(entry.book())) {
+                        treasureTagBuilder.addOptionalTag(scrollTagKey);
+                    }
                 }
                 for (var group : entry.weaponGroups()) {
                     var weaponGroupTagKey = SpellTags.weapon(namespace, group.toString().toLowerCase());

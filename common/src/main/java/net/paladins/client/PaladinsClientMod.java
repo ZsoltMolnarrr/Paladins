1b701db22e63e84a2b0588c058b9073cdffb6f0d
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2026-08-20 21:35:39 +0200

    AzureLibArmor is back

--- common/src/main/java/net/paladins/client/PaladinsClientMod.java
+++ common/src/main/java/net/paladins/client/PaladinsClientMod.java
@@ -1,7 +1,7 @@
 package net.paladins.client;
 
-import net.rpg_foundation.armor_api.client.ArmorRenderers;
-import net.rpg_foundation.armor_api.client.GeoArmorRenderer;
+import mod.azure.azurelibarmor.common.render.armor.AzArmorRenderer;
+import mod.azure.azurelibarmor.common.render.armor.AzArmorRendererRegistry;
 import net.paladins.client.armor.PaladinArmorRenderer;
 import net.paladins.client.armor.PriestArmorRenderer;
 import net.paladins.client.effect.DivineProtectionRenderer;
@@ -18,6 +18,7 @@ import net.spell_engine.api.spell.fx.ParticleGroupBuilder.Batches;
 import net.spell_engine.client.util.Color;
 import net.spell_engine.fx.SpellEngineParticles;
 
+import java.util.function.Supplier;
 
 public class PaladinsClientMod {
     public static void init() {
@@ -55,15 +56,15 @@ public class PaladinsClientMod {
         // The barrier's after-translucent render pass (BarrierEntityRenderer.renderAfterTranslucent) is
         // likewise wired per-platform from each client entrypoint's native world-render event.
 
-        registerArmorRenderer(Armors.paladinArmorSet_t1, PaladinArmorRenderer.paladin());
-        registerArmorRenderer(Armors.paladinArmorSet_t2, PaladinArmorRenderer.crusader());
-        registerArmorRenderer(Armors.paladinArmorSet_t3, PaladinArmorRenderer.netheriteCrusader());
-        registerArmorRenderer(Armors.priestArmorSet_t1, PriestArmorRenderer.priest());
-        registerArmorRenderer(Armors.priestArmorSet_t2, PriestArmorRenderer.prior());
-        registerArmorRenderer(Armors.priestArmorSet_t3, PriestArmorRenderer.netheritePrior());
+        registerArmorRenderer(Armors.paladinArmorSet_t1, PaladinArmorRenderer::paladin);
+        registerArmorRenderer(Armors.paladinArmorSet_t2, PaladinArmorRenderer::crusader);
+        registerArmorRenderer(Armors.paladinArmorSet_t3, PaladinArmorRenderer::netheriteCrusader);
+        registerArmorRenderer(Armors.priestArmorSet_t1, PriestArmorRenderer::priest);
+        registerArmorRenderer(Armors.priestArmorSet_t2, PriestArmorRenderer::prior);
+        registerArmorRenderer(Armors.priestArmorSet_t3, PriestArmorRenderer::netheritePrior);
     }
 
-    private static void registerArmorRenderer(Armor.Set set, GeoArmorRenderer renderer) {
-        ArmorRenderers.register(renderer, set.head, set.chest, set.legs, set.feet);
+    private static void registerArmorRenderer(Armor.Set set, Supplier<AzArmorRenderer> armorRendererSupplier) {
+        AzArmorRendererRegistry.register(armorRendererSupplier, set.head, set.chest, set.legs, set.feet);
     }
 }
--- common/src/main/java/net/paladins/client/armor/PriestArmorRenderer.java
+++ common/src/main/java/net/paladins/client/armor/PriestArmorRenderer.java
@@ -1,26 +1,28 @@
 package net.paladins.client.armor;
 
+import mod.azure.azurelibarmor.common.render.armor.AzArmorRenderer;
+import mod.azure.azurelibarmor.common.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelibarmor.common.render.layer.AzArmorTrimLayer;
 import net.minecraft.util.Identifier;
 import net.paladins.PaladinsMod;
-import net.rpg_foundation.armor_api.client.GeoArmorRenderer;
 
-public final class PriestArmorRenderer {
-    private PriestArmorRenderer() { }
-
-    public static GeoArmorRenderer priest() {
-        return make("priest_robes", "priest_robe");
+public class PriestArmorRenderer extends AzArmorRenderer {
+    public static PriestArmorRenderer priest() {
+        return new PriestArmorRenderer("priest_robes", "priest_robe");
     }
-    public static GeoArmorRenderer prior() {
-        return make("priest_robes", "prior_robe");
+    public static PriestArmorRenderer prior() {
+        return new PriestArmorRenderer("priest_robes", "prior_robe");
     }
-    public static GeoArmorRenderer netheritePrior() {
-        return make("priest_robes", "netherite_prior_robe");
+    public static PriestArmorRenderer netheritePrior() {
+        return new PriestArmorRenderer("priest_robes", "netherite_prior_robe");
     }
 
-    private static GeoArmorRenderer make(String modelName, String textureName) {
-        return GeoArmorRenderer.of(
+    public PriestArmorRenderer(String modelName, String textureName) {
+        super(AzArmorRendererConfig.builder(
                 Identifier.of(PaladinsMod.ID, "geo/" + modelName + ".geo.json"),
                 Identifier.of(PaladinsMod.ID, "textures/armor/" + textureName + ".png"))
-                .trim(Identifier.of(PaladinsMod.ID, "armor/trim/" + textureName + "_generic"), false);
+                .addRenderLayer(new AzArmorTrimLayer(Identifier.of(PaladinsMod.ID, "armor/trim/" + textureName + "_generic"), false))
+                .build()
+        );
     }
 }
